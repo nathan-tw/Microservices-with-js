632118d7304afd6dfe397f4f200bf94d6958005c
nathan-tw <106306027@nccu.edu.tw>
2020-09-24 13:32:42 -0400

    add event bus

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -22,9 +23,22 @@ app.post('/posts', (req, res) => {
     title
   };
 
+  axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id, title
+    }
+  });
+
   res.status(201).send(posts[id]);
 });
 
+
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  res.send({});
+})
 app.listen(4000, () => {
   console.log('Listening on 4000');
 });
